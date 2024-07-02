3f863cacd4e075fad064f781b69299319fa1eafd
Pranamya Kulal <spacedust26@gmail.com>
2024-07-02 20:34:17 +0530

    reusable card wrapper done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils"
-import { LoginButton } from "@/components/auth/login-button.";
+import { LoginButton } from "@/components/auth/login-button";
 
 const font = Poppins({
   subsets: ["latin"],
